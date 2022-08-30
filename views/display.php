c70e68496467b14f78c961b7fcb90f741c5fad51
samsms <you@example.com>
2022-08-30 12:05:26 +0300

    upd

--- views/display.php
+++ views/display.php
@@ -28,7 +28,7 @@ if($_SERVER['REMOTE_ADDR']=="127.0.0.1"){
 
 $baseurl='/property-rivercourt/';}
 else{
-	$baseurl="";
+	$baseurl="/";
 }
 $htmlheaders='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
