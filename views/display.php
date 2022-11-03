ddbbeafc9c543bb4f8eafae3c159fb765ce406ee
KAILIKIA123 <aaronmwirigi2018@gmail.com>
2022-11-03 09:32:28 +0300

    disp

--- views/display.php
+++ views/display.php
@@ -26,10 +26,10 @@ $endinput='</div>';
 //application base /root folder
 if($_SERVER['REMOTE_ADDR']!="::1"&&$_SERVER['REMOTE_ADDR']!="127.0.0.1"){
 
-$baseurl='/property-rivercourt/';
+$baseurl='/';
 }
 else{
-	$baseurl="/";
+	$baseurl="/property-rivercourt/'";
 }
 $htmlheaders='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
