f6511dae439567eee388098d37a8783113bae51b
silvano36 <silvanussigei1996@gmail.com>
2022-11-10 12:10:36 +0300

    silvano

--- modules/functions.php
+++ modules/functions.php
@@ -1,8 +1,8 @@
 <?php
 //die("dd");
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+// ini_set('display_errors',1);
+// ini_set('display_startup_errors',1);
+// error_reporting(-1);
 @session_start();
 @include '../includes/database.php';
 @header("Access-Control-Allow-Origin: *");
