e8f7492793f40d826999af42979712f85b9c3afe
samsms <you@example.com>
2023-02-14 15:24:38 +0300

    updated

--- includes/config.php
+++ includes/config.php
@@ -14,10 +14,10 @@ defined('DB_NAME')   ? null : define("DB_NAME", "techsava_rivercourt");
 }else{
    //die("online");
    defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-    defined('DB_USER')   ? null : define("DB_USER","silvano");
-    defined('DB_PASS')   ? null : define("DB_PASS", "access");
+    defined('DB_USER')   ? null : define("DB_USER","sam");
+    defined('DB_PASS')   ? null : define("DB_PASS", "samsaf");
     //defined('DB_NAME')   ? null : define("DB_NAME", "techsava_property_htest");
-    defined('DB_NAME')   ? null : define("DB_NAME", "techsava_rivercourt");
+    defined('DB_NAME')   ? null : define("DB_NAME", "prop_management");
 }
 date_default_timezone_set("Africa/Nairobi");
 //$root="https://localhost/property-rivercourt-test/";
