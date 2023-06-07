02054f778b8555aa8019ee784bfbd65c1fefb413
samsms <you@example.com>
2023-06-07 16:43:38 +0300

    update

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ error_reporting(1);
 if($_SERVER['REMOTE_ADDR']!="::1"&&$_SERVER['REMOTE_ADDR']!="127.0.0.1"){
 
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "techsava_rivercourt_property");
+defined('DB_USER')   ? null : define("DB_USER", "root");
 defined('DB_PASS')   ? null : define("DB_PASS", "Trymenot#123$");
 //defined('DB_NAME')   ? null : define("DB_NAME", "techsava_property_htest");
 defined('DB_NAME')   ? null : define("DB_NAME", "rivercourt_prop_management");
